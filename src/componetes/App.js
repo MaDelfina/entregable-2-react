93998af4a54ff58fd87d06fd0b15969a292729e7
MaDelfina <103047943+MaDelfina@users.noreply.github.com>
2022-07-20 16:24:34 -0300

    Agregando estilos

---
+++ src/componetes/App.js
@@ -0,0 +1,17 @@
+import NavBar from "./NavBar";
+
+function App() {
+    return (
+        <>
+            <NavBar/>
+            <main>
+
+            </main>
+            <footer>
+
+            </footer>
+        </>
+    )
+}
+
+export default App;
---
+++ src/componetes/NavBar.js
@@ -0,0 +1,14 @@
+function NavBar() {
+    return (
+        <header id="header">
+            <h1 className="logo">BT | twins</h1>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Productos</a>
+                <a href="#">contacto</a>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar;
